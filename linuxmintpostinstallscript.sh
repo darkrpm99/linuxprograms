d0ad9f751abc68bc8df1b139781891cad3c1de79
darkrpm99 <70522464+darkrpm99@users.noreply.github.com>
2020-08-31 16:08:02 +0200

    Add files via upload

--- linuxmintpostinstallscript.sh
+++ linuxmintpostinstallscript.sh
@@ -3,10 +3,6 @@
 #actualizar paquetes y sistema
 sudo apt update && sudo apt upgrade
 
-#Firefox
-
-sudo apt install firefox
-
 #google chrome
 sudo touch /etc/apt/sources.list.d/google-chrome.list
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
@@ -30,19 +26,35 @@ wget https://go.skype.com/skypeforlinux-64.deb
 sudo dpkg -i skypeforlinux-64.deb
 
 #Thunderbird
+
 sudo add-apt-repository ppa:mozillateam/ppa
 sudo apt update
 sudo apt install thunderbird
 
 #Python 3
+
 sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt update 
 sudo apt install python3.6
 
 #filezilla
 
+sudo apt update
 sudo apt install filezilla
 
 #Vlc player
 
+sudo apt update
 sudo apt install vlc
+
+#Qbitorrent
+
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+sudo apt update
+sudo apt install qbittorrent
+
+#gimp
+
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt update
+sudo apt install gimp
