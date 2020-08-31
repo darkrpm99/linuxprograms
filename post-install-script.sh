c0759c8eb28b07f3f8187aaefa361522f92f4198
darkrpm99 <70522464+darkrpm99@users.noreply.github.com>
2020-08-31 20:18:47 +0200

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt upgrade
 sudo touch /etc/apt/sources.list.d/google-chrome.list
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 wget https://dl.google.com/linux/linux_signing_key.pub
-signing key chrome sudo apt-key add linux_signing_key.pub
+sudo apt-key add linux_signing_key.pub
 sudo apt update
 sudo apt install google-chrome-stable
 
